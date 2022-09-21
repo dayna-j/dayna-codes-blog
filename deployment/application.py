2d8e23d9ed33999d9265f345fc80c6cffaaea589
Dayna Blackwell <Dayna.Blackwell@bestwestern.com>
2022-09-20 17:30:55 -0700

    Initial commit for Dayna Codes blog

---
+++ deployment/application.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+application = Flask(__name__)
+
+# from app import routes
+
+@application.route("/")
+def hello_world():
+    return "<p>Hello, Wordld!</p>"
+
+@application.route("/earth")
+def hello_earth():
+    return "<p>Hello, earth!</p>"
+
+@application.route("/mars")
+def hello_mars():
+    return "<p>Hello, mars!</p>"
+
+if __name__ == "__main__":
+    application.run(port=5000, debug=True)
