e07f2858fa28262d32308aac961ba1fff6c37ae3
Dayna Blackwell <Dayna.Blackwell@bestwestern.com>
2022-09-22 22:16:27 -0700

    added routes.py and template folter

--- application.py
+++ application.py
@@ -2,23 +2,8 @@ from flask import Flask
 
 application = Flask(__name__)
 
-# from app import routes
-
-@application.route("/")
-def hello_world():
-    return "<p>Hello, Wordld!</p>"
-
-@application.route("/earth")
-def hello_earth():
-    return "<p>Hello, earth!</p>"
-
-@application.route("/mars")
-def hello_mars():
-    return "<p>Hello, mars!</p>"
-
-@application.route("/venus")
-def hello_venus():
-    return "<p>Hello, venus!</p>"
+import routes
 
 if __name__ == "__main__":
     application.run(port=5000, debug=True)
+
---
+++ routes.py
@@ -0,0 +1,17 @@
+from application import application
+
+@application.route("/")
+def hello_world():
+    return "<p>Hello, Wordld!</p>"
+
+@application.route("/earth")
+def hello_earth():
+    return "<p>Hello, earth!</p>"
+
+@application.route("/mars")
+def hello_mars():
+    return "<p>Hello, mars!</p>"
+
+@application.route("/venus")
+def hello_venus():
+    return "<p>Hello, venus!</p>"
